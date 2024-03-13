8210f9f1790c7185e749ae99350ceaab7f94288c
Shivam24S <shivamshrivastav8055@gmail.com>
2024-03-13 21:55:29 +0530

    lazy loading added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import BlogPage, { loader as postsLoader } from "./pages/Blog";
+// import BlogPage, { loader as postsLoader } from "./pages/Blog";
 import HomePage from "./pages/Home";
-import PostPage, { loader as postLoader } from "./pages/Post";
+// import PostPage, { loader as postLoader } from "./pages/Post";
 import RootLayout from "./pages/Root";
+import { lazy, Suspense } from "react";
+
+// now here im loading page and loader function using lazy loading
+// what is lazy loading ? it means load particular data when it needed using this out large complex website can become very fast
+// lazy loading always return promise
+
+// using this code we can load our component
+const BlogPage = lazy(() => import("./pages/Blog"));
+
+const PostPage = lazy(() => import("./pages/Post"));
 
 const router = createBrowserRouter([
   {
@@ -17,8 +27,30 @@ const router = createBrowserRouter([
       {
         path: "posts",
         children: [
-          { index: true, element: <BlogPage />, loader: postsLoader },
-          { path: ":id", element: <PostPage />, loader: postLoader },
+          {
+            index: true,
+            element: (
+              <Suspense
+                fallback={<h1 style={{ textAlign: "center" }}>Loading....</h1>}
+              >
+                <BlogPage />
+              </Suspense>
+            ),
+            loader: () =>
+              import("./pages/Blog").then((module) => module.loader()),
+          },
+          {
+            path: ":id",
+            element: (
+              <Suspense
+                fallback={<h1 style={{ textAlign: "center" }}>Loading...</h1>}
+              >
+                <PostPage />
+              </Suspense>
+            ),
+            loader: (meta) =>
+              import("./pages/Post").then((module) => module.loader(meta)),
+          },
         ],
       },
     ],
